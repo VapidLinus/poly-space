e76f117de68fa21eb57c843096a6a76341a0e7e7
Linus Närkling-Larsson <VapidLinus@gmail.com>
2015-03-20 12:01:18 +0100

    Raw circle script

--- Assets/Scripts/Shapes/Circle.cs
+++ Assets/Scripts/Shapes/Circle.cs
@@ -9,9 +9,8 @@ public class Circle : MonoBehaviour
 	private bool relativeCorners = true;
 	private bool fillBack = false;
 	private bool isMeshDirty = true;
-	public float testy = 10;
 
-	private int edges = 64;
+	private int edges;
 	public int Edges
 	{
 		get { return edges; }
@@ -22,19 +21,15 @@ public class Circle : MonoBehaviour
 		}
 	}
 
-	private float radius = 1f;
-	public float Radius { get { return radius; } set { radius = Mathf.Max(value, float.Epsilon); } }
-
-	public bool FillBack
+	private float[] radius;
+	public float[] Radius
 	{
-		get { return fillBack; }
+		get { return radius; }
 		set
 		{
-			if (fillBack != value)
-			{
-				isMeshDirty = true;
-				fillBack = value;
-			}
+			if (value.Length != edges) throw new System.ArgumentException("Radius length must be equal to edge count.");
+			radius = value;
+			isMeshDirty = true;
 		}
 	}
 
@@ -46,33 +41,53 @@ public class Circle : MonoBehaviour
 
 		// Default unlit color
 		renderer.material = new Material(Shader.Find("Unlit/Color"));
+
+		// Setup variables
+		edges = 32;
+		radius = new float[edges];
+		SetRadius(1f);
 	}
 
 	void Update()
 	{
+		// If the mesh needs to be rebuilt
 		if (isMeshDirty)
 			UpdateMesh();
 	}
 
+	/// <summary>Set the entire circle's radius.</summary>
+	/// <param name="radius">Radius for circle.</param>
+	public void SetRadius(float radius)
+	{
+		for (int i = 0; i < this.radius.Length; i++)
+		{
+			this.radius[i] = radius;
+		}
+	}
+
 	private void UpdateMesh()
 	{
-		// isMeshDirty = false;
+		// Mesh no longer needs to be rebuilt
+		isMeshDirty = false;
 
+		// Initialize arrays
 		Vector3[] vertices = new Vector3[edges + 1];
 		int[] triangles = new int[vertices.Length * 3];
+
+		// Center point
 		vertices[0] = Vector2.zero;
 
+		// Build vertices
 		for (int i = 1; i < vertices.Length; i++)
 		{
-			float angle = (i / (float)edges) * 360f;
+			float radians = (i / (float)edges) * 360f * Mathf.Deg2Rad;
 
-			Vector2 direction = new Vector2(
-				Mathf.Sin(angle * Mathf.Deg2Rad),
-				Mathf.Cos(angle * Mathf.Deg2Rad));
-
-			vertices[i] = direction * radius * (1 + Mathf.PerlinNoise(Time.time, i / testy));
+			vertices[i] = new Vector2(
+				Mathf.Sin(radians),
+				Mathf.Cos(radians)) * radius[i - 1];
 		}
 
+		// Build triangles
 		for (int i = 0; i < edges; i++)
 		{
 			triangles[i * 3 + 0] = 0;
